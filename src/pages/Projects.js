d9cdad698a511a91249d8026ae2278ea8a429816
jaylordbubucayu <57398270+jaylordbubucayu@users.noreply.github.com>
2022-04-26 22:03:36 +0800

    final commit

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -17,11 +17,18 @@ const Projects = () => {
     return (
     <div className="projects">
     
-      <Splide aria-label="..." data-splide='{"perPage":1,"arrows":false,"wheel":true,"releaseWheel":true,"flickPower":600}'>
-  <SplideSlide>
+    
       <div class="projects-container">
 
           <div class="project-details">
+          <svg width="135" height="56" viewBox="0 0 185 96" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M70.5 95C68.5 93.8334 65 90.7 67 87.5C69.5 83.5 70.5 83 73.5 83.5C75.9 83.9 78.5 85.3333 79.5 86L80 86.5L78 77.5H89.5" stroke="black" stroke-width="0.5"/>
+<path d="M102 59.5C97.8333 62 89.5 68.1 89.5 72.5C89.5 76.9 89.5 77 89.5 76.5V78L135 77C132.667 77.6667 127.6 79.3 126 80.5C124.609 81.5435 122.492 82.8289 121.5 84.3563M125 94.5C123.5 93.5 121.5 89 121 86.5C120.848 85.7391 121.066 85.0245 121.5 84.3563M125 94.5C126.5 95.5 130 95.5 131.5 94.5C133 93.5 135.5 91.5 135 89C134.5 86.5 133.5 81.5 131 81.5C128.5 81.5 121.5 83.2125 121.5 84.3563M125 94.5C127 95.0757 136.833 95.4066 141.5 95.5H185" stroke="black" stroke-width="0.5"/>
+<path d="M68.5 50H142" stroke="black" stroke-width="0.5"/>
+<path d="M61.5 31.5L103 34.5L142.5 37.5" stroke="black" stroke-width="0.5"/>
+<path d="M0 95H75.5C77.3333 94.1667 80.9 91.5 80.5 87.5C80.1 83.5 78.6667 79.1667 78 77.5H70.5C69.6667 77.3333 68.5 76.2 70.5 73C73 69 82 65 83.5 64C84.7 63.2 85.3333 61.3333 85.5 60.5H72.5L54.3261 16.5M50.2917 9L53.5 14.5L54.3261 16.5M50.2917 9L50 8.49998C48 5.83332 43.7 0.499983 42.5 0.499983C41 0.499983 37.5 -1.70469e-05 37.5 1.49998C37.5 2.99998 37 4.5 40 6C43 7.5 44 8.49997 46 8.49998C47.6 8.5 49.5278 8.83333 50.2917 9ZM54.3261 16.5H60C66.1667 18.3333 79 22 81 22C81.3417 22 82.2253 22.0467 83.5 22.1274M83.5 22.1274C91.5509 22.6367 115.205 24.5 116.5 24.5C116.712 24.5 117.424 24.5622 118.5 24.6701M83.5 22.1274C83.3333 23.4182 83.4 26.9 85 30.5C87 35 91.5 49.5 92 54.5C92.4 58.5 92.1667 59.1667 92 59C95.5 59.1667 102.4 59.5 102 59.5C101.5 59.5 115.5 58.5 118 58.5C118.282 58.5 118.8 58.4867 119.5 58.463M118.5 24.6701C123.527 25.1742 136.499 26.6763 143.5 27.5C143.167 34.3333 142.5 48.2 142.5 49C142.5 49.8 141.167 55 140.5 57.5C134.774 57.7863 123.761 58.3185 119.5 58.463M118.5 24.6701L119.5 41V58.463" stroke="black" stroke-width="0.5"/>
+</svg>
+
               <h1>City Online Market</h1>
               <p>A e-commerce website for buying essentials good within the City. This is a Capstone Project and also participated in a Start-up competition.</p> 
           
@@ -29,15 +36,38 @@ const Projects = () => {
         </div>
 
         <div class="project-image">
-        <img src={process.env.PUBLIC_URL + 'images/projects/projectcom.jpg'} alt="Image 1"/>
+        <svg width="389" height="218" viewBox="0 0 389 218" fill="none">
+      
+     
+<rect x="39.5" y="0.5" width="310" height="200" rx="4.5" stroke="black"/>
+
+
+<rect x="48.5" y="11.5" width="291" height="181" fill="white" stroke="black"/>
+
+<image x="48.5" y="-1.5" width="291"  height="200" href={process.env.PUBLIC_URL + 'images/projects/projectcovid.jpg'}/>
+
+<path d="M1.5 206C1.58607 206 1.67213 206 1.75817 206M1.75817 206C310.743 205.6 388.333 205.5 388.5 205.5V213L0.5 212.5V206L1.75817 206Z" stroke="black"/>
+<path d="M1 213.5C2.33333 214.833 7.3 217.5 16.5 217.5C25.7 217.5 258.667 217.5 374 217.5C378.167 217.167 386.7 216 387.5 214" stroke="black"/>
+<path d="M167 206.5C170.5 211.5 171 211 172 211.5C172.8 211.9 202.333 211.667 217 211.5C217.5 211.667 218.8 211.5 220 209.5C221.2 207.5 221.833 206 222 205.5H167V206" stroke="black"/>
+<path d="M350.5 192V205.5" stroke="black"/>
+<path d="M39 181V205" stroke="black"/>
+
+
+
+</svg>
+
+    
         </div>
       </div>
    
-  </SplideSlide>
-  <SplideSlide>
+  
       <div class="projects-container">
-
+     
           <div class="project-details">
+          <svg width="75" height="56" viewBox="0 0 75 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M54.5 55L67 20V15C68.1667 13 70.8 9 72 9C73.5 9 74 9.5 74 10.5C74 11.5 73 13.5 71.5 15.5C70 17.5 69 19 68 19C67.2 19 65.3333 20 64.5 20.5L46 34L37 12C36.3333 10.8333 35.1 7.9 35.5 5.5C36 2.5 37 1.9297e-06 38 0.500002C39 1 40 3 40 4.5C40 6 40 8.5 40 9C40 9.5 38.5 12 38.5 12.5C38.5 12.9 37.1667 15 36.5 16L30 34L29.5 34.5L7 15.5H5.5C3.83333 13.8333 0.5 10.2 0.5 9C0.5 7.5 1 3.5 3.5 6.5C6 9.5 7.5 11.5 7.5 12C7.5 12.4 7.5 14.5 7.5 15.5L9 20L23 52.5L48.5 54" stroke="black" stroke-width="0.5"/>
+</svg>
+
               <h1>Tracking The Crown</h1>
               <p>This Website is to track the covid in every country. This Web-application using API from the Api.covid19api. 
              </p>
@@ -45,16 +75,39 @@ const Projects = () => {
         </div>
 
         <div class="project-image">
-        <img src={process.env.PUBLIC_URL + 'images/projects/projectcovid.jpg'} alt="Image 1"/>
+        <svg width="389" height="218" viewBox="0 0 389 218" fill="none">
+      
+     
+      <rect x="39.5" y="0.5" width="310" height="200" rx="4.5" stroke="black"/>
+      
+      
+      <rect x="48.5" y="11.5" width="291" height="181" fill="white" stroke="black"/>
+      
+      <image x="48.5" y="-1.5" width="291"  height="200" href={process.env.PUBLIC_URL + 'images/projects/projectcom.jpg'} />
+      
+      <path d="M1.5 206C1.58607 206 1.67213 206 1.75817 206M1.75817 206C310.743 205.6 388.333 205.5 388.5 205.5V213L0.5 212.5V206L1.75817 206Z" stroke="black"/>
+      <path d="M1 213.5C2.33333 214.833 7.3 217.5 16.5 217.5C25.7 217.5 258.667 217.5 374 217.5C378.167 217.167 386.7 216 387.5 214" stroke="black"/>
+      <path d="M167 206.5C170.5 211.5 171 211 172 211.5C172.8 211.9 202.333 211.667 217 211.5C217.5 211.667 218.8 211.5 220 209.5C221.2 207.5 221.833 206 222 205.5H167V206" stroke="black"/>
+      <path d="M350.5 192V205.5" stroke="black"/>
+      <path d="M39 181V205" stroke="black"/>
+      
+      
+      
+      </svg>
+       
         </div>
       </div>
    
-  </SplideSlide>
-
-  <SplideSlide>
+  
       <div class="projects-container">
 
           <div class="project-details">
+          <svg width="101" height="51" viewBox="0 0 131 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.25" y="0.25" width="130.5" height="70.5" rx="12.75" stroke="black" stroke-width="0.5"/>
+<rect x="5.25" y="6.25" width="120.5" height="59.5" rx="12.75" stroke="black" stroke-width="0.5"/>
+<path d="M19.7173 55V47.7273H24.4474V48.8317H21.0348V50.8061H24.2024V51.9105H21.0348V53.8956H24.4759V55H19.7173ZM31.7681 47.7273V55H30.5962L27.1694 50.0462H27.109V55H25.7915V47.7273H26.9705L30.3938 52.6847H30.4577V47.7273H31.7681ZM32.8841 48.8317V47.7273H38.6866V48.8317H36.4387V55H35.1319V48.8317H32.8841ZM39.8052 55V47.7273H44.5353V48.8317H41.1227V50.8061H44.2903V51.9105H41.1227V53.8956H44.5637V55H39.8052ZM45.8794 55V47.7273H48.6067C49.1654 47.7273 49.6342 47.8243 50.013 48.0185C50.3941 48.2126 50.6818 48.4848 50.8759 48.8352C51.0724 49.1832 51.1706 49.5893 51.1706 50.0533C51.1706 50.5196 51.0712 50.9245 50.8723 51.2678C50.6758 51.6087 50.3858 51.8726 50.0023 52.0597C49.6188 52.2443 49.1477 52.3366 48.589 52.3366H46.6465V51.2429H48.4114C48.7381 51.2429 49.0056 51.1979 49.214 51.108C49.4223 51.0156 49.5762 50.8819 49.6756 50.7067C49.7774 50.5291 49.8283 50.3113 49.8283 50.0533C49.8283 49.7952 49.7774 49.575 49.6756 49.3928C49.5738 49.2081 49.4187 49.0684 49.2104 48.9737C49.0021 48.8767 48.7334 48.8281 48.4043 48.8281H47.1969V55H45.8794ZM49.6365 51.7045L51.437 55H49.9668L48.1983 51.7045H49.6365Z" fill="black"/>
+</svg>
+
               <h1>KeyCode Generator </h1>
               <p>This is a simple keycode generator. It is a simple project to detect key code in any key pressed. 
              </p>
@@ -62,14 +115,31 @@ const Projects = () => {
         </div>
 
         <div class="project-image">
-        <img src={process.env.PUBLIC_URL + 'images/projects/projectkey.jpg'} alt="Image 1"/>
+        <svg width="389" height="218" viewBox="0 0 389 218" fill="none">
+      
+     
+      <rect x="39.5" y="0.5" width="310" height="200" rx="4.5" stroke="black"/>
+      
+      
+      <rect x="48.5" y="11.5" width="291" height="181" fill="white" stroke="black"/>
+      
+      <image x="48.5" y="-1.5" width="291"  height="200" href={process.env.PUBLIC_URL + 'images/projects/projectkey.jpg'} />
+      
+      <path d="M1.5 206C1.58607 206 1.67213 206 1.75817 206M1.75817 206C310.743 205.6 388.333 205.5 388.5 205.5V213L0.5 212.5V206L1.75817 206Z" stroke="black"/>
+      <path d="M1 213.5C2.33333 214.833 7.3 217.5 16.5 217.5C25.7 217.5 258.667 217.5 374 217.5C378.167 217.167 386.7 216 387.5 214" stroke="black"/>
+      <path d="M167 206.5C170.5 211.5 171 211 172 211.5C172.8 211.9 202.333 211.667 217 211.5C217.5 211.667 218.8 211.5 220 209.5C221.2 207.5 221.833 206 222 205.5H167V206" stroke="black"/>
+      <path d="M350.5 192V205.5" stroke="black"/>
+      <path d="M39 181V205" stroke="black"/>
+      
+      
+      
+      </svg>
+       
         </div>
       </div>
-   
-  </SplideSlide>
 
-  
-</Splide>
+
+
     </div>)
 
 }
